f3c1198dbdc3016a3af792b15db3a1bd58deaf08
liulingfeng <liulingfeng@mistong.com>
2019-09-02 17:36:13 +0800

    删除APP多余代码

--- app/src/main/java/com/lxs/fastclick/App.kt
+++ app/src/main/java/com/lxs/fastclick/App.kt
@@ -1,13 +1,8 @@
 package com.lxs.fastclick
 
 import android.app.Application
-import android.util.Log
-import android.view.View
-import android.view.ViewGroup
 import com.lxs.fastclick.utils.FastClickUtil
-import com.lxs.fastclick.utils.ReflectUtils
 import com.lxs.fastclick.utils.ToastUtils
-import java.util.*
 
 /**
  * @author liuxiaoshuai
@@ -21,29 +16,5 @@ class App : Application() {
 
         ToastUtils.init(this)
         FastClickUtil.init(this)
-    }
-
-    /**
-     * 深度遍历
-     */
-    private fun depthTravelView(rootView: View?) {
-        val queue = ArrayDeque<View>()
-        queue.addLast(rootView)
-        while (!queue.isEmpty()) {
-            val temp = queue.first
-            if (temp is ViewGroup) {
-                for (item in 0 until temp.childCount) {
-                    queue.addLast(temp.getChildAt(item))
-                }
-            }
-
-            val listener = ReflectUtils.getOnClickListener(temp)
-            if (listener != null) {
-                Log.e("德玛", "你好${temp.javaClass.name}")
-            } else {
-                Log.e("德玛", temp.javaClass.name)
-            }
-            queue.pollFirst()
-        }
     }
 }
